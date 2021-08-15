22c7906921a801375d6f964834824d750d49de8c
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2021-08-15 17:27:49 +0200

    Add scrape_img.py (WIP)

---
+++ scrape_img.py
@@ -0,0 +1,13 @@
+"""
+https://www.reddit.com/r/learnpython/comments/p4s9sx/can_someone_help_me_debug_my_code_i_am_building/
+"""
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted by user.")
