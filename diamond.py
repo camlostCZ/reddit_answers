69abbcfa7f3dbce8c54a4b87b116d71f8304046f
jiri-novak-cz <jiri.novak+GIT@gmail.com>
2022-05-21 07:37:26 +0200

    Add diamond.py

---
+++ diamond.py
@@ -0,0 +1,33 @@
+"""
+https://www.reddit.com/r/learnpython/comments/uuf3bu/can_i_print_something_only_if_it_meets_a_condition/
+"""
+
+from string import ascii_uppercase
+
+
+def get_upper_triangle(letter: str) -> list[str]:
+    result = []
+    size = (ord(letter) - ord('A')) + 1
+    for i, item in enumerate(ascii_uppercase[:size]):
+        line = [' '] * size
+        line[size - i - 1] = item
+        result.append("".join(line + list(reversed(line))[1:]))
+    return result
+
+
+def get_diamond(letter: str) -> list[str]:
+    triangle = get_upper_triangle(letter)
+    return triangle + list(reversed(triangle))[1:]
+
+
+def main() -> None:
+    letter = input("Enter a letter: ").upper()
+    for line in get_diamond(letter):
+        print(line)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted by user.")
